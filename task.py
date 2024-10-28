dad5165062d430d8a3e4c039b14ccc355dbc957c
Haneen Alaa <112430100+haneenalaa465@users.noreply.github.com>
2024-10-28 18:15:23 +0300

    Changes

--- task.py
+++ task.py
@@ -1,5 +1,9 @@
 class Task:
-    def __init__(self,ID, description, duration):
+    def __init__(self,ID, description, duration, deadline, dependencies):
         self.ID = ID
         self.description = description
         self.duration = duration
+        self.deadline = deadline
+        self.dependencies = dependencies
+    
+    
