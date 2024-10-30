8c32d30e4eb3f297fcffd342bd88a1a54d37abcf
Haneen Alaa <112430100+haneenalaa465@users.noreply.github.com>
2024-10-30 09:15:09 +0300

    update poblem class

--- problem.py
+++ problem.py
@@ -1,5 +1,25 @@
 class Problem:
     def __init__(self, tasks):
         self.tasks = tasks
+        self.schedule = []
+        self.today = 0
 
-    def 
+    def action(schedule):
+        # list of possible tasks w/o dependencies with their costs
+        possible_routes = []
+        for i in range(len(self.tasks)):
+            if tasks[i].dependcies == []:
+                cost = tasks[i].deadline - tasks[i].duration - self.today
+                possible_routes.append([tasks[i], cost])
+        
+
+        
+
+
+            
+    
+    def result(state):
+        if state == 1:
+            print(f"Task {} choosed")
+        else:
+            print(f"Task {} ignored")
