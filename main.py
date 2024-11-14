74d441b5a84320516e7fd45cac665b03e4fbbc03
Tasneem-Mohammed <tasneem.abdelgaleel@gmail.com>
2024-11-14 09:36:22 +0200

    Typo error

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from node import Node
 from task import Task
 
 task1 = Task(1, "run", 2, 3, [])
-print(task1.getID(), task1.getDescription(), task1.getDuration(), task1.getDeadline(), task1.getDependies())
+print(task1.getID(), task1.getDescription(), task1.getDuration(), task1.getDeadline(), task1.getDependencies())
