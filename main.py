ef7c27889d95af6fa45ecf82d513515519465190
Farha-A <s-farha.shady@zewailcity.edu.eg>
2024-11-14 10:25:42 +0200

    added task visualization function

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from node import Node
 from task import Task
 
 task1 = Task(1, "run", 2, 3, [])
-print(task1.getID(), task1.getDescription(), task1.getDuration(), task1.getDeadline(), task1.getDependencies())
+task1.task_vis()
+task2 = Task(2, "walk", 2, 5, [])
+task2.task_vis()
+task3 = Task(3, "crawl", 2, 7, [1])
+task3.task_vis()
