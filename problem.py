8cf0ccf1aa44f5bc46889d02b44a9721ac39d2f5
Farha-A <s-farha.shady@zewailcity.edu.eg>
2024-11-14 09:42:31 +0200

    added functionality for chosen task getting removed from other tasks dependencies

--- problem.py
+++ problem.py
@@ -1,3 +1,5 @@
+from task import Task
+
 class Problem:
     def __init__(self, tasks):
         self.tasks = tasks
@@ -19,6 +21,11 @@ class Problem:
         min_task_cost = min(possible_routes, key=possible_routes.get)
         self.today += min_task_cost.get_duration()
         self.schedule.append(min_task_cost)
+        for task in self.tasks:
+            if min_task_cost.getID() in task.getDependencies():
+                deps = task.getDependencies().remove(min_task_cost.getID())
+                task.setDependencies(deps)
+
 
     # returns schedule    
     def result(self):
