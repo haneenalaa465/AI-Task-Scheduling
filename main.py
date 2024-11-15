ba1adc0528a1da1113c7404ce4c7810b898a8430
Tasneem-Mohammed <tasneem.abdelgaleel@gmail.com>
2024-11-15 10:08:15 +0200

    Typo errors in Problem class and in accessing the methods + some test in the main function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from problem import Problem
 from node import Node
 from task import Task
+from ucs import UCS
 
-task1 = Task(1, "run", 2, 3, [])
-print(task1.getID(), task1.getDescription(), task1.getDuration(), task1.getDeadline(), task1.getDependencies())
+# task1 = Task(1, "run", 2, 3, [])
+# print(task1.getID(), task1.getDescription(), task1.getDuration(), task1.getDeadline(), task1.getDependencies())
+def main():
+    task1 = Task("A", "Task A", 2, 10, [])
+    task2 = Task("B", "Task B", 3, 8, ["A"])
+    task3 = Task("C", "Task C", 1, 6, ["A"])
+    task4 = Task("D", "Task D", 2, 12, ["B", "C"])
+    tasks = [task1, task2, task3, task4]
+
+    problem = Problem(tasks, init_state = task1)
+    # ucs = UCS(tree=None, init_state=task1, problem=problem)  
+    # ucs.findPath()
+
+if __name__ == "__main__":
+    main()
--- problem.py
+++ problem.py
@@ -1,6 +1,8 @@
+# from task import Task
 class Problem:
-    def __init__(self, tasks):
+    def __init__(self, tasks, init_state):
         self.tasks = tasks
+        self.init_state = init_state
         self.schedule = []
         self.today = 0
 
@@ -8,8 +10,8 @@ class Problem:
     def step_cost(self):
         possible_routes = dict()
         for i in range(len(self.tasks)):
-            if self.tasks[i].dependcies == []:
-                cost = self.tasks[i].deadline - self.tasks[i].duration - self.today
+            if self.tasks[i].getDependencies() == []:
+                cost = self.tasks[i]._deadline - self.tasks[i].getDuration() - self.today
                 possible_routes[self.tasks[i]] = cost
         return possible_routes
 
@@ -17,7 +19,7 @@ class Problem:
     def action(self):
         possible_routes = self.step_cost()
         min_task_cost = min(possible_routes, key=possible_routes.get)
-        self.today += min_task_cost.get_duration()
+        self.today += min_task_cost.getDuration()
         self.schedule.append(min_task_cost)
         for task in self.tasks:
             if min_task_cost.getID() in task.getDependencies():
