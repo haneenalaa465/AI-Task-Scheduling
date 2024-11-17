19dee68f6fa9f2ccb74fda7a46e4ec19ab91a869
Haneen Alaa <112430100+haneenalaa465@users.noreply.github.com>
2024-11-17 22:07:12 +0200

    Changes

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def main():
     # task5 = Task(5, "Task 5", 2, 20, [])
 
     # example 2
-    task1 = Task(1, "Task 1", 2, 6, [])  
-    task2 = Task(2, "Task 2", 4, 8, [1, 3])  
-    task3 = Task(3, "Task 3", 3, 5, [1])  
-    task4 = Task(4, "Task 4", 2, 7, [2, 5])  
-    task5 = Task(5, "Task 5", 5, 9, [3])
+    # task1 = Task(1, "Task 1", 2, 6, [])  
+    # task2 = Task(2, "Task 2", 4, 8, [1, 3])  
+    # task3 = Task(3, "Task 3", 3, 5, [1])  
+    # task4 = Task(4, "Task 4", 2, 7, [2, 5])  
+    # task5 = Task(5, "Task 5", 5, 9, [3])
 
     # example 3
     # task1 = Task("A", "Task A", 2, 3, [])
@@ -26,8 +26,20 @@ def main():
     # task5 = Task("E", "Task E", 1, 5, ["B", "D"])
     # tasks = [task1, task2, task3, task4, task5]
 
+    tasks = [
+    Task(1, "Design System Architecture", 2, 3, []),
+    Task(2, "Develop Backend Services", 4, 6, [1]),
+    Task(3, "Set Up Database Infrastructure", 3, 5, [1]),
+    Task(4, "Design Frontend Wireframes", 2, 4, [1]),
+    Task(5, "Implement User Authentication", 2, 6, [2, 3]),
+    Task(6, "Integrate Backend with Database", 2, 7, [3]),
+    Task(7, "Build Frontend Components", 3, 8, [4, 5]),
+    Task(8, "Perform API Testing", 2, 9, [6, 7]),
+    Task(9, "Prepare Technical Documentation", 2, 10, [5]),
+    Task(10, "Security Testing", 3, 11, [8, 9])
+]
+
     # show one algorithm result at a time
-    tasks = [task1, task2, task3, task4, task5]
     problem = Problem(tasks, 0)
     print("\nbfs, dfs, ids, ucs, greedy, a_star, hillclimb, simulated_annealing, genetic")
     algo = input("Select algorithm (type as above):")
